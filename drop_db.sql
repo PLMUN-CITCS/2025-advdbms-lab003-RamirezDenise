33251a52f72965c8749a56e4e15a2bcb57938562
RamirezDenise <ramirezdenise_bsit@plmun.edu.ph>
2025-02-22 13:19:31 +0800

    Update drop_db.sql

--- drop_db.sql
+++ drop_db.sql
@@ -1,9 +1,2 @@
--- Step 3: (Optional) Drop the database if needed. 
--- Use with extreme caution!
--- The IF EXISTS clause prevents an error if the database doesn't exist.
-
--- Select the database you are going to drop
 USE UniversityDB;
-
--- Drop the database if it exists
 DROP DATABASE IF EXISTS UniversityDB;
